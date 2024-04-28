4308c38b4cc1d2251ba7e00d61b9c32b663a23c5
无名路人 <mumoe@foxmail.com>
2024-04-28 15:29:39 +0800

    修改配置与说明

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { fileURLToPath, URL } from "node:url";
-
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import AutoImport from "unplugin-auto-import/vite";
@@ -8,24 +7,33 @@ import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: true,
-    https: true,
-  },
-  plugins: [
-    vue(),
-    basicSsl(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const { VITE_APP_TYPE } = env;
+
+  // 定义 basicSsl 插件的条件逻辑
+  const sslPlugin = VITE_APP_TYPE === "replit" ? basicSsl() : undefined;
+
+  return {
+    server: {
+      host: true,
+      https: VITE_APP_TYPE !== "replit", // 根据条件设置 https
     },
-  },
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      sslPlugin,
+    ].filter(Boolean), // 确保只有真值被包含
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
 });
